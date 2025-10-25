165a5a5e2c2fcdc8249ab33cf4b917861cf58cef
Jhonathan <jhonathandasilva2002@gmail.com>
2025-10-25 18:48:12 -0300

    Aumenta a quantidade máxima de caracteres na coluna "servico" para 255

--- server/config.php
+++ server/config.php
@@ -41,7 +41,7 @@ try {
             `senha` VARCHAR(255) NOT NULL,
             `whatsAppLink` VARCHAR(27) NOT NULL,
             `tipoUsuario` VARCHAR(11) NOT NULL,
-            `servico` VARCHAR(30) NOT NULL,
+            `servico` VARCHAR(255) NOT NULL,
             `fotoPerfil` varchar(255) DEFAULT NULL
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
     ");
